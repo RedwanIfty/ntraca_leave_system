996478fab00b997e3887090602cca345039b7889
Rakibul Islam <sheikh2rin@gmail.com>
2022-11-05 03:41:43 -0600

    superadmin cannot view their personal leave info

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -77,23 +77,23 @@
                     </a>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-sm-3 "></div>
-                <div class="col-sm-6">
-                    <div class="card" style="background-color:#683091; border-radius: 20px">
-                        <div class="card-body" style="text-align: center">
-                            <div style="text-align: center">
-                                <h3 style="text-align: center; color:#ffff !important" class="text-primary">ব্যক্তিগত
-                                </h3>
+
+            @if ($myLeave)
+                <div class="row">
+                    <div class="col-sm-3 "></div>
+                    <div class="col-sm-6">
+                        <div class="card" style="background-color:#683091; border-radius: 20px">
+                            <div class="card-body" style="text-align: center">
+                                <div style="text-align: center">
+                                    <h3 style="text-align: center; color:#ffff !important" class="text-primary">ব্যক্তিগত
+                                    </h3>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-2"></div>
-                <div class="col-sm-4">
-                    <a href="/leave/list">
+                <div class="row">
+                    <div class="col-sm-4 ml-auto">
                         <div class="card" style="background-color:#8bc643;  border-radius: 20px">
                             <div class="card-body">
                                 <div class="d-flex align-items-center mb-3">
@@ -102,92 +102,50 @@
                                             <i style="color: saddlebrown" class="bx bx-user"></i>
                                         </span>
                                     </div>
-                                    <h4 class="font-size-54 mb-0">সর্বমোট ছুটি নিয়েছে</h4>
+
+                                    <h2 style="color: #ffff !important; padding-right:5px;font-size:51px">
+                                        {{ $myLeave->sum('approved_total_days') }} </h2>
+                                    <h4 style="color: #ffff !important;" class="font-size-54 mt-4"> দিন</h4>
                                 </div>
-                                <div class="text-muted mt-4">
-                                    <h4>21 জন</h4>
+                                <div class="text-muted">
+                                    <h4 style="color: #ffff !important;padding-left: 49px;font-size: 27px;"> ভোগকৃত ছুটি
+                                    </h4>
                                 </div>
                             </div>
                         </div>
-                    </a>
-                </div>
-                <div class="col-sm-4">
-                    <a href="/join/list">
-                        <div class="card" style="background-color:#8bc643;  border-radius: 20px">
+                    </div>
+                    <div class="col-sm-4  mr-auto">
+                        <div class="card" style="background-color:#8bc643;   border-radius: 20px">
                             <div class="card-body">
                                 <div class="d-flex align-items-center mb-3">
                                     <div class="avatar-xs mr-3">
                                         <span class="avatar-title rounded-circle bg-soft-primary text-primary font-size-18">
-                                            <i class="bx bx-user"></i>
+                                            <i style="color: saddlebrown" class="bx bx-user"></i>
                                         </span>
                                     </div>
 
-                                    <h4 class="font-size-54 mb-0">সর্বমোট অফিস করছে</h4>
+                                    <h2 style="color: #ffff !important; padding-right:5px;font-size:51px;">
+                                        {{ 20 - $myLeave->sum('approved_total_days') }} </h2>
+                                    <h4 style="color: #ffff !important;" class="font-size-54 mt-4">দিন </h4>
                                 </div>
-                                <div class="text-muted mt-4">
-                                    <h4> 87 জন</h4>
+                                <div class="text-muted">
+                                    <h4 style="color: #ffff !important;padding-left: 49px;font-size: 27px;"> অবশিষ্ট ছুটি
+                                        </i></h4>
                                 </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-
-
-                <div class="col-sm-2"></div>
-                <div class="col-sm-2"></div>
-
-                <div class="col-sm-4">
-                    <div class="card" style="background-color:#8bc643;  border-radius: 20px">
-                        <div class="card-body">
-                            <div class="d-flex align-items-center mb-3">
-                                <div class="avatar-xs mr-3">
-                                    <span class="avatar-title rounded-circle bg-soft-primary text-primary font-size-18">
-                                        <i style="color: saddlebrown" class="bx bx-user"></i>
-                                    </span>
-                                </div>
-
-                                <h2 style="color: #ffff !important; padding-right:5px;font-size:51px">
-                                    {{$myLeave->sum('approved_total_days')}} </h2>
-                                <h4 style="color: #ffff !important;" class="font-size-54 mt-4"> দিন</h4>
-                            </div>
-                            <div class="text-muted">
-                                <h4 style="color: #ffff !important;padding-left: 49px;font-size: 27px;"> ভোগকৃত ছুটি
-                                  </h4>
                             </div>
                         </div>
                     </div>
+
+
                 </div>
-                <div class="col-sm-4">
-                    <div class="card" style="background-color:#8bc643;   border-radius: 20px">
-                        <div class="card-body">
-                            <div class="d-flex align-items-center mb-3">
-                                <div class="avatar-xs mr-3">
-                                    <span class="avatar-title rounded-circle bg-soft-primary text-primary font-size-18">
-                                        <i style="color: saddlebrown" class="bx bx-user"></i>
-                                    </span>
-                                </div>
+            @endif
 
-                                <h2 style="color: #ffff !important; padding-right:5px;font-size:51px;">
-                                    {{20-$myLeave->sum('approved_total_days')}} </h2>
-                                <h4 style="color: #ffff !important;" class="font-size-54 mt-4">দিন </h4>
-                            </div>
-                            <div class="text-muted">
-                                <h4 style="color: #ffff !important;padding-left: 49px;font-size: 27px;"> অবশিষ্ট ছুটি
-                                    </i></h4>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-
-            </div>
         </div>
     </div>
 
     <div class="row" align="center">
-
         <div class="col-sm-4 mx-auto">
-            <a href="{{route('pending.application')}}">
+            <a href="{{ route('pending.application') }}">
                 <div class="card" style="background-color: #683091; border-radius: 20px">
                     <div class="card-body" style="text-align: center; font-size:x-large;color:#ffff;font:bolder">
                         জমাকৃত আবেদন
@@ -198,7 +156,7 @@
     </div>
     <div class="row">
         <div class="col-sm-4 mx-auto">
-            <a href="{{route('application.form')}}">
+            <a href="{{ route('application.form') }}">
                 <div class="card" style="background-color: #683091; border-radius: 20px">
                     <div class="card-body" style="text-align: center; font-size:x-large;color:#ffff;font:bolder">
                         আবেদন
